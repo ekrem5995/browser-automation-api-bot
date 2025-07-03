319505a4c893c50f65911922ab45ab148a94dd8c
ekrem5995 <96389359+ekrem5995@users.noreply.github.com>
2025-07-03 03:24:14 +0300

    Added full README, requirements.txt, and detailed code documentation

--- main.py
+++ main.py
@@ -1,17 +1,17 @@
 
 import sys
-import os
+import os                                                           
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), ".")))
-
+ 
 import scripts.step1_call_api as sca
-from utils.browser import open_browser_and_submit_form
+from utils.browser import open_browser_and_submit_form  #Imports the URL generator and browser automation.
 
 def run_all():
-    candidates = sca.get_urls_for_all("data/input.csv")
+    candidates = sca.get_urls_for_all("data/input.csv")#Starts the process and gets all enriched candidates.
 
     for candidate in candidates:
-        print(f"[INFO] Submitting for {candidate['name']}")
-        open_browser_and_submit_form(candidate["submission_url"], candidate)
+        print(f"[INFO] Submitting for {candidate['name']}")          
+        open_browser_and_submit_form(candidate["submission_url"], candidate) #Loops through and submits forms.
 
 if __name__ == "__main__":
     run_all()
